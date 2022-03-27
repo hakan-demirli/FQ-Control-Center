ab30fa09bae2cddce6384fa5ca133e0bf1c40679
hakan-demirli <emre.demirli@metu.edu.tr>
2022-03-27 13:26:07 +0300

    Separates testbenches

--- quartus/ip/simple_memory/simple_memory_tb.v
+++ quartus/ip/simple_memory/simple_memory_tb.v
@@ -7,74 +7,76 @@
 module simple_memory_tb();
 
 
+parameter ADDRESS_SIZE = 4;
+parameter DELAY = 5;
+integer C_DATA[ADDRESS_SIZE-1:0];
+integer mismatch_error = 0;
+
+integer var;
+initial begin
+  $display("Initializing test data randomly");
+  for(var=0; var<ADDRESS_SIZE; var = var + 1) begin
+    C_DATA[var] = $urandom();
+  end
+end
+
+reg [ADDRESS_SIZE-1: 0] address;
+reg          chipselect;
 reg          clk;
 reg          reset_n;
+reg          write_n;
+reg [ 31: 0] writedata;
+wire [ 31: 0] readdata;
 
-
-
-reg [15:0]prescalar;
-wire tx;
-reg [4:0]cfg;
-wire o_data_ready;
-wire [10: 0] o_data;
-reg  [ 7: 0] i_data;
-uart_rx uart_rx_0(
-    .i_clk(clk),
-    .i_rst(!reset_n),
-    .i_prescalar(prescalar),
-    .i_rx(tx),
-    .i_cfg(cfg), 
-    .o_data_ready(o_data_ready),
-    .o_data(o_data)
+simple_memory #(.ADDRESS_SIZE(ADDRESS_SIZE)) sm (
+    .address(address),
+    .chipselect(chipselect),
+    .clk(clk),
+    .reset_n(reset_n),
+    .write_n(write_n),
+    .writedata(writedata),
+    .readdata(readdata)
 );
 
-reg [7:0] random_data;
-wire busy;
-reg data_send;
-uart_tx uart_tx_0(
-    .i_clk(clk),
-    .i_rst(!reset_n),
-    .i_prescalar(prescalar),
-    .i_data_send(data_send),
-    .i_data(random_data),
-    .i_cfg(cfg), 
-    .o_busy(busy),
-    .o_tx(tx)
-);
 
 always  #10 clk <= !clk;
 
 integer i;
 initial begin
-
-
     $display("Initializing inputs");
+    address = 0;
+    chipselect = 0;
     clk = 0;
-    reset_n = 0;
-
-    cfg = 5'b11_11_1;
-    prescalar = 677;
-    data_send = 0;
+    reset_n = 1;
+    write_n = 1;
+    writedata = 0;
 
     // wait for couple of cycles
     repeat (10) @(posedge clk);
-    reset_n = 1;
 
     $display("Writing to memory");
-    random_data = 10'b01_0101_1000;
-    data_send = 1;
-    @(posedge busy); // wait until tx starts
-    data_send = 0;
-    
-    @(posedge o_data_ready); // wait until tx stops
-    $display("random_data: %b",random_data);
-    $display("o_data: %b",o_data);
-    if(o_data[7:0] == random_data[7:0])
-        $display("SUCCESS");
-    else
+    for(i=0;i<ADDRESS_SIZE;i=i+1)begin
+        address = i;
+        writedata = C_DATA[i];
+        chipselect = 1;
+        write_n = 0; // write operation
+        @(posedge clk);
+    end
+
+    $display("Reading from memory");
+    for(i=0;i<ADDRESS_SIZE;i=i+1)begin
+        address = i;
+        chipselect = 1; 
+        write_n = 1; // Read operation
+        @(posedge clk);
+        if(C_DATA[i] != readdata) begin
+            $display("RW Not equal, time: %t, read: %h, real: %h",$time,C_DATA[i],readdata);
+        end
+    end
+    if(mismatch_error)
         $display("FAILED");
-
-
+    else
+        $display("SUCCESS");
     $stop;
 end
 
---
+++ quartus/ip/simple_memory/uart_tb.v
@@ -0,0 +1,81 @@
+// synthesis translate_off
+`timescale 1ns / 1ps
+// synthesis translate_on
+`default_nettype none
+
+
+module simple_memory_tb();
+
+
+reg          clk;
+reg          reset_n;
+
+
+
+reg [15:0]prescalar;
+wire tx;
+reg [4:0]cfg;
+wire o_data_ready;
+wire [10: 0] o_data;
+reg  [ 7: 0] i_data;
+uart_rx uart_rx_0(
+    .i_clk(clk),
+    .i_rst(!reset_n),
+    .i_prescalar(prescalar),
+    .i_rx(tx),
+    .i_cfg(cfg), 
+    .o_data_ready(o_data_ready),
+    .o_data(o_data)
+);
+
+reg [7:0] random_data;
+wire busy;
+reg data_send;
+uart_tx uart_tx_0(
+    .i_clk(clk),
+    .i_rst(!reset_n),
+    .i_prescalar(prescalar),
+    .i_data_send(data_send),
+    .i_data(random_data),
+    .i_cfg(cfg), 
+    .o_busy(busy),
+    .o_tx(tx)
+);
+
+always  #10 clk <= !clk;
+
+integer i;
+initial begin
+
+
+    $display("Initializing inputs");
+    clk = 0;
+    reset_n = 0;
+
+    cfg = 5'b11_11_1;
+    prescalar = 677;
+    data_send = 0;
+
+    // wait for couple of cycles
+    repeat (10) @(posedge clk);
+    reset_n = 1;
+
+    $display("Writing to memory");
+    random_data = 10'b01_0101_1000;
+    data_send = 1;
+    @(posedge busy); // wait until tx starts
+    data_send = 0;
+    
+    @(posedge o_data_ready); // wait until tx stops
+    $display("random_data: %b",random_data);
+    $display("o_data: %b",o_data);
+    if(o_data[7:0] == random_data[7:0])
+        $display("SUCCESS");
+    else
+        $display("FAILED");
+
+
+    $stop;
+end
+
+endmodule
